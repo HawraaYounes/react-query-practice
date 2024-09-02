c3f3e406bde4fc4b67b9fbe247c2dd09692f65a7
HawraaYounes <hawraayounes0@gmail.com>
2024-09-02 20:50:51 +0300

    handled errors using fetch

--- src/components/DataFetching/User.jsx
+++ src/components/DataFetching/User.jsx
@@ -2,24 +2,27 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const fetchUser = async ({ queryKey }) => {
-  const [_, {userId}] = queryKey;
+  const [_, { userId }] = queryKey;
   const response = await fetch(`https://reqres.in/api/users/${userId}`);
+  if(!response.ok){
+    throw new Error('User not found!')
+  }
   return response.json();
 };
 
 const User = () => {
-  const userId = 1;
+  const userId = 0;
   const { isLoading, isError, error, data } = useQuery({
-    queryKey: ["users", {userId}],
+    queryKey: ["users", { userId }],
     queryFn: fetchUser,
   });
-  console.log("USER DATA", data);
+  console.log(isLoading, isError, error, data);
 
   if (isLoading) {
     return <h1> Loading ...</h1>;
   }
   if (isError) {
-    return <h1> Something wrong occured! {error.message}</h1>;
+    return <h1>{error.message}</h1>;
   }
   return (
     <div>
